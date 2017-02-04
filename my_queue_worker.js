e7397b579ac8f080deb0b7aff47982f9fb18979a
Diego Garcia <haxaco@gmail.com>
2017-02-03 23:44:05 -0800

    First take at handling webhooks

--- my_queue_worker.js
+++ my_queue_worker.js
@@ -87,3 +87,8 @@ app.get('/', function(request, response) {
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
+
+app.post('/hook', function(request, response) {
+    console.log('request is ',request);
+    response.send('OK');
+});

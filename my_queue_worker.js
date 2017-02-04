71762a017da844d3d87673ce25b7d6b6055977d7
Diego Garcia <haxaco@gmail.com>
2017-02-04 00:39:41 -0800

    Setting http headers

--- my_queue_worker.js
+++ my_queue_worker.js
@@ -23,6 +23,25 @@ var ref = admin.database().ref('work-queue');
 var app     = express();
 app.set('port', (process.env.PORT || 5000));
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 //initialize firebase-queue worker
 var queue = new Queue(ref, function(data, progress, resolve, reject) {
     // Read and process task data
